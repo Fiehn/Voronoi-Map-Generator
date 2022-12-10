d70141f7a1a1add40e89060cb906ba2389b9c4e4
Fiehn <81577064+Fiehn@users.noreply.github.com>
2022-12-10 15:03:51 -0500

    Mouse movements and zoom, needs bounding

--- SFML attempt/main.cpp
+++ SFML attempt/main.cpp
@@ -6,10 +6,7 @@
 #include <time.h>
 #include <cstdlib>
 
-// For random use:
-// rand() << 15 | rand();
-// It will shift the first random number 15 bits and then insert another random number in those 15 bits, thereby generating 32 bits
-
+// There might be a problem with neighbors.. There are holes in the height map??
 
 int main()
 {
@@ -19,7 +16,7 @@ int main()
 
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "SFML");
     window.setFramerateLimit(27); // For now there is no reason to have even this high framerate
-    sf::View view = window.getDefaultView();
+    
     
     // as a hash table????? would be better, perhaps?
     std::vector<sf::Vector2f> points;
@@ -40,7 +37,7 @@ int main()
 
     clock_t start = clock();
     random_height_gen(map, 5, 0.04, 0.02, 0.005, 1.0, "Random");
-    smooth_height(map,0.09,3,"Random");
+    smooth_height(map,0.09,5,"Random");
     clock_t end = clock();
     std::cout << double(end - start) / CLOCKS_PER_SEC;
     
@@ -67,7 +64,14 @@ int main()
         
         bgMap.draw(T);
     }
-    
+
+    sf::Vector2f oldPos;
+    bool moving = false;
+
+    float zoom = 1;
+    // Retrieve the window's default view
+    sf::View view = window.getDefaultView();
+
     bgMap.display();
     sf::Sprite background(bgMap.getTexture());
 
@@ -75,32 +79,69 @@ int main()
     {
         sf::Event event;
         while (window.pollEvent(event))
-        {            
-            if (event.type == sf::Event::Closed)
+        {      
+            switch (event.type) {
+            case sf::Event::Closed:
                 window.close();
+            case sf::Event::MouseButtonPressed:
+                // Mouse button is pressed, get the position and set moving as active
+                if (event.mouseButton.button == 0) {
+                    moving = true;
+                    oldPos = window.mapPixelToCoords(sf::Vector2i(event.mouseButton.x, event.mouseButton.y));
+                }
+                break;
+                
+            case sf::Event::MouseButtonReleased:
+                    
+                // Mouse button is released, no longer move
+                if (event.mouseButton.button == 0) {
+                    moving = false;
+                }
+                break;
+                    
+            case sf::Event::MouseMoved:
+            {
+                // Ignore mouse movement unless a button is pressed (see above)
+                if (!moving) {
+                    break;
+                }
+                // Determine the new position in world coordinates
+                const sf::Vector2f newPos = window.mapPixelToCoords(sf::Vector2i(event.mouseMove.x, event.mouseMove.y));
+                // Determine how the cursor has moved
+                // Swap these to invert the movement direction
+                const sf::Vector2f deltaPos = oldPos - newPos;
 
-            /*if (event.type == sf::Event::Resized)
-            {
-                sf::FloatRect view(0, 0, event.size.width, event.size.height);
-                window.setView(sf::View(view));
-            }*/
-            
-            // https://stackoverflow.com/questions/65458106/c-sfml-how-to-adjust-game-view-via-mouse-movements
-            if (event.type == sf::Event::MouseButtonPressed)
-            {
-                view.zoom(0.9f);
+                // Move our view accordingly and update the window
+                view.setCenter(view.getCenter() + deltaPos);
                 window.setView(view);
+
+                // Save the new position as the old one
+                // We're recalculating this, since we've changed the view
+                oldPos = window.mapPixelToCoords(sf::Vector2i(event.mouseMove.x, event.mouseMove.y));
+                break;
             }
-            /*
-            if (event.type == sf::Keyboard::A)
-            {
-                view.zoom(1.f);
-                window.setView(window.getDefaultView());
+            case sf::Event::MouseWheelScrolled:
+                // Ignore the mouse wheel unless we're not moving
+                if (moving){break;}
+
+                if (event.mouseWheelScroll.delta <= -1)
+                {
+                    zoom = std::min(2.f, zoom + .1f);
+                }
+                else if (event.mouseWheelScroll.delta >= 1)
+                {
+                    zoom = std::max(.1f, zoom - .1f);
+                }
+
+                // Update our view
+                view.setSize(window.getDefaultView().getSize()); 
+                view.zoom(zoom); 
+                window.setView(view);
+                break;
+            
             }
-            if (event.type == sf::Event::KeyPressed)
-            {
-                view.move(100.f, 100.f);
-            }*/
+            
+            
         }
 
         
@@ -114,3 +155,31 @@ int main()
     return 0;
 }
 
+
+
+//if (event.type == sf::Event::MouseButtonPressed)
+//{
+//    bgMap.clear();
+//    for (int i = 0; i < map.size(); i++) {
+//        int water = 1;
+//        if (map[i].height < 1) { water = 0; }
+//        sf::Color color(255 * water, 255 * water, 255 * (1 - water), (sf::Uint8)std::ceil(255 * map[i].height)); // Here it loses data in conversion which is fine but hives warning so ask it to do it
+//        if (map[i].vertex.size() == 0) { continue; };
+//        sf::VertexArray T(sf::TriangleFan, map[i].vertex.size() + 1);
+//
+//        for (int j = 0; j < map[i].vertex.size(); j++) {
+//            T[j].position = voronoi_points[map[i].vertex[j]];
+//            T[j].color = color;
+//
+//        }
+//
+//        T[map[i].vertex.size()].position = voronoi_points[map[i].vertex[0]];
+//        T[map[i].vertex.size()].color = color;
+//
+//        bgMap.draw(T);
+//    }
+//
+//    bgMap.display();
+//    sf::Sprite background(bgMap.getTexture());
+//
+//}
