2ac6491d266cc17d423dc7ec638b73afdd5bd866
Fiehn <81577064+Fiehn@users.noreply.github.com>
2022-12-16 15:57:49 -0500

    View limits

--- SFML attempt/main.cpp
+++ SFML attempt/main.cpp
@@ -22,8 +22,8 @@ int main()
 
 
     clock_t start = clock();
-    random_height_gen(map.cells, 5, 0.04, 0.02, 0.005, 1.0, "Random");
-    smooth_height(map.cells,0.09,5,"Random");
+    random_height_gen(map.cells, 7, 0.04, 0.02, 0.01, 1.0, "Random");
+    smooth_height(map.cells,0.09,8,"Random");
     clock_t end = clock();
     std::cout << double(end - start) / CLOCKS_PER_SEC;
     
@@ -35,20 +35,20 @@ int main()
     for (int i = 0; i < map.cells.size(); i++) {
         int water = 1;
         if (map.cells[i].height < 0.50) { water = 0; }
-        sf::Color color(128*water, 255 * water, 255 * (1 - water), (sf::Uint8)std::ceil(255 * map.cells[i].height)); // Here it loses data in conversion which is fine but hives warning so ask it to do it
-        if (map.cells[i].vertex.size() == 0) { continue; };
-        sf::VertexArray T(sf::TriangleFan, map.cells[i].vertex.size() + 1);
+        sf::Color color(128*water, 255 * water, 255 * (1 - water), 55+(sf::Uint8)std::ceil(200 * map.cells[i].height)); // Here it loses data in conversion which is fine but hives warning so ask it to do it
+if (map.cells[i].vertex.size() == 0) { continue; };
+sf::VertexArray T(sf::TriangleFan, map.cells[i].vertex.size() + 1);
 
-        for (int j = 0; j < map.cells[i].vertex.size(); j++) {
-            T[j].position = map.voronoi_points[map.cells[i].vertex[j]];
-            T[j].color = color;
+for (int j = 0; j < map.cells[i].vertex.size(); j++) {
+    T[j].position = map.voronoi_points[map.cells[i].vertex[j]];
+    T[j].color = color;
 
-        }
+}
 
-        T[map.cells[i].vertex.size()].position = map.voronoi_points[map.cells[i].vertex[0]];
-        T[map.cells[i].vertex.size()].color = color;
-        
-        bgMap.draw(T);
+T[map.cells[i].vertex.size()].position = map.voronoi_points[map.cells[i].vertex[0]];
+T[map.cells[i].vertex.size()].color = color;
+
+bgMap.draw(T);
     }
 
     sf::Vector2f oldPos;
@@ -65,7 +65,7 @@ int main()
     {
         sf::Event event;
         while (window.pollEvent(event))
-        {      
+        {
             switch (event.type) {
             case sf::Event::Closed:
                 window.close();
@@ -76,15 +76,15 @@ int main()
                     oldPos = window.mapPixelToCoords(sf::Vector2i(event.mouseButton.x, event.mouseButton.y));
                 }
                 break;
-                
+
             case sf::Event::MouseButtonReleased:
-                    
+
                 // Mouse button is released, no longer move
                 if (event.mouseButton.button == 0) {
                     moving = false;
                 }
                 break;
-                    
+
             case sf::Event::MouseMoved:
             {
                 // Ignore mouse movement unless a button is pressed (see above)
@@ -97,40 +97,61 @@ int main()
                 // Swap these to invert the movement direction
                 const sf::Vector2f deltaPos = oldPos - newPos;
 
-                // Move our view accordingly and update the window
-                view.setCenter(view.getCenter() + deltaPos);
+                sf::Vector2f view_center = view.getCenter() + deltaPos;
+
+                // Make sure that the view is contained inside the map
+                if ((view.getCenter().x + deltaPos.x) + zoom * windowWidth / 2 > windowWidth || (view.getCenter().x + deltaPos.x) - zoom * windowWidth / 2 < 0) {
+                    view_center.x = view.getCenter().x;
+                }
+                if ((view.getCenter().y + deltaPos.y) + zoom * windowHeight / 2 > windowHeight || (view.getCenter().y + deltaPos.y) - zoom * windowHeight / 2 < 0) {
+                    view_center.y = view.getCenter().y;
+                }
+
+                view.setCenter(view_center);
                 window.setView(view);
 
                 // Save the new position as the old one
-                // We're recalculating this, since we've changed the view
                 oldPos = window.mapPixelToCoords(sf::Vector2i(event.mouseMove.x, event.mouseMove.y));
                 break;
             }
             case sf::Event::MouseWheelScrolled:
                 // Ignore the mouse wheel unless we're not moving
-                if (moving){break;}
+                if (moving) { break; }
 
                 if (event.mouseWheelScroll.delta <= -1)
                 {
-                    zoom = std::min(2.f, zoom + .1f);
+                    zoom = std::min(1.f, zoom + .07f);
                 }
                 else if (event.mouseWheelScroll.delta >= 1)
                 {
-                    zoom = std::max(.1f, zoom - .1f);
+                    zoom = std::max(.1f, zoom - .07f);
                 }
 
                 // Update our view
-                view.setSize(window.getDefaultView().getSize()); 
-                view.zoom(zoom); 
+                view.setSize(window.getDefaultView().getSize());
+                view.zoom(zoom);
+
+                // Make sure that the view is contained inside the map
+                sf::Vector2f view_center = view.getCenter();
+                if ((view.getCenter().x) + zoom * windowWidth / 2 > windowWidth) {
+                    view_center.x = windowWidth - zoom * windowWidth / 2;
+                }
+                if ((view.getCenter().x) - zoom * windowWidth / 2 < 0) {
+                    view_center.x = zoom * windowWidth / 2;
+                }
+                if ((view.getCenter().y) + zoom * windowHeight / 2 > windowHeight) { 
+                    view_center.y = windowHeight - zoom * windowHeight / 2;
+                }
+                if (view.getCenter().y - zoom * windowHeight / 2 < 0) {
+                    view_center.y = zoom * windowHeight / 2;
+                }
+                view.setCenter(view_center);
+               
                 window.setView(view);
                 break;
-            
             }
-            
-            
         }
 
-        
         window.clear();
         
         window.draw(background);
